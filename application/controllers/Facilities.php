b23780b8b0d9549e5f261386da1a274b6749b658
din1030 <din1030@gmail.com>
2015-09-18 02:30:47 +0800

    test commit

--- application/controllers/Facilities.php
+++ application/controllers/Facilities.php
@@ -1,11 +1,12 @@
 <?php
+
 class Facilities extends CI_Controller
 {
     public function __construct()
     {
-    	parent::__construct();
+        parent::__construct();
         $this->load->model('Facility');
-        log_message('debug', "Facilities Controller Initialized");
+        log_message('debug', 'Facilities Controller Initialized');
     }
 
     public function index()
@@ -22,8 +23,7 @@ class Facilities extends CI_Controller
         $query = $this->Facility->join_exh_title('facilities.id, facilities.title as fac_title, exhibitions.title as exh_title, facilities.ibeacon_id');
         $result_array = $query->result_array();
         $facilities = array();
-        foreach ($result_array as $fac_row)
-        {
+        foreach ($result_array as $fac_row) {
             $manage_ctrl = "<button id='edit_fac_btn_".$fac_row['id']."' type='button' class='btn btn-primary edit-fac-btn' data-toggle='modal' data-fac-id='".$fac_row['id']."'>編輯</button>&nbsp;";
             $manage_ctrl .= "<button id='del_fac_btn_".$fac_row['id']."' type='button' class='btn btn-danger del-fac-btn' data-toggle='modal' data-fac-id='".$fac_row['id']."'>刪除</button>";
             array_push($fac_row, $manage_ctrl);
@@ -34,18 +34,17 @@ class Facilities extends CI_Controller
 
         $this->table->clear();
         $this->table->set_heading(array('ID', '設施名稱', '所屬展覽', '連結iBeacon', '管理'));
-        $tmpl = array ( 'table_open'  => '<table id="fac_list" data-toggle="table" data-striped="true">',
-                        'heading_cell_start'  => '<th data-sortable="true">');
+        $tmpl = array('table_open' => '<table id="fac_list" data-toggle="table" data-striped="true">',
+                    'heading_cell_start' => '<th data-sortable="true">' );
         $this->table->set_template($tmpl);
 
         return $facilities;
     }
 
-
     public function get_fac_add_form()
     {
         $this->load->model('Exhibition');
-        $data['exhibitions'] = $this->Exhibition->prepare_for_dropdwon();;
+        $data['exhibitions'] = $this->Exhibition->prepare_for_dropdwon();
 
         $this->load->model('Ibeacon');
         $data['ibeacons'] = $this->Ibeacon->prepare_for_dropdwon();
@@ -59,7 +58,7 @@ class Facilities extends CI_Controller
         $data['facility'] = $this->Facility->find_by_id($fac_id);
 
         $this->load->model('Exhibition');
-        $data['exhibitions'] = $this->Exhibition->prepare_for_dropdwon();;
+        $data['exhibitions'] = $this->Exhibition->prepare_for_dropdwon();
 
         $this->load->model('Ibeacon');
         $data['ibeacons'] = $this->Ibeacon->prepare_for_dropdwon();
@@ -71,12 +70,9 @@ class Facilities extends CI_Controller
     {
         $this->form_validation->set_rules('fac_title', '名稱', 'required');
 
-        if ($this->form_validation->run() == FALSE)
-        {
+        if ($this->form_validation->run() == false) {
             $this->load->view('facility/add');
-        }
-        else
-        {
+        } else {
             $data = array(
                 'owner_id' => $this->input->post('fac_owner'),
                 'exh_id' => $this->input->post('fac_exh'),
@@ -84,7 +80,7 @@ class Facilities extends CI_Controller
                 'description' => $this->input->post('fac_description'),
                 'main_pic' => $this->input->post('fac_main_pic'),
                 'push_content' => $this->input->post('fac_push'),
-                'ibeacon_id' => $this->input->post('fac_ibeacon')
+                'ibeacon_id' => $this->input->post('fac_ibeacon'),
             );
             $this->Facility->create($data);
 
@@ -94,61 +90,58 @@ class Facilities extends CI_Controller
 
     public function edit_facility_action()
     {
-        $this->form_validation->set_rules('fac_title', '標題', 'required');
-        if ($this->form_validation->run() == FALSE)
-        {
-            $this->load->view('facility/edit');  // 應該要改成沒過就不會送出
-        }
-        else
-        {
-            if (empty($_FILES['fac_main_pic'])) {
-                echo json_encode(['error'=>'No files found for upload.']);
-                // or you can throw an exception
-                return; // terminate
-
-            } else {
-                $images = $_FILES['fac_main_pic'];
-                print_r($images);
-                $config['upload_path'] = 'user_uploads';
-                // $config['file_name'] = 'test';
-        		$config['allowed_types'] = 'gif|jpg|png';
-        		// $config['max_size']	= '100000';
-        		// $config['max_width']  = '1024';
-        		// $config['max_height']  = '768';
-
-        		$this->upload->initialize($config);
-
-                if ( !$this->upload->do_multiple_upload('fac_main_pic'))
-        		{
-                    log_message('debug', "Upload failed.");
-        			// echo json_encode(['error'=>'Upload failed.']);
-                    $error = array('error' => $this->upload->display_errors());
-        			$this->load->view('facility/fac_manage', $error);
-
-                    // return;
-        		}
-        		else
-        		{
-                    log_message('debug', "Upload successful.");
-        			$data = array('upload_data' => $this->upload->data());
-                    print_r($data);
-
-                    // echo json_encode(['error'=>'Upload successful.']);
-        			// $this->load->view('upload_success'，$data);
-        		}
-        	}
-
-
+        $this->form_validation->set_rules('fac_title', '設施名稱', 'required');
+        if ($this->form_validation->run() == false) {
+            echo validation_errors();
+        } else {
+            // update facility information
             $fac_obj = $this->Facility->find($this->input->post('fac_id'));
             $fac_obj->exh_id = $this->input->post('fac_exh');
             $fac_obj->title = $this->input->post('fac_title');
             $fac_obj->description = $this->input->post('fac_description');
-            $fac_obj->main_pic = $this->input->post('fac_main_pic');
+            // $fac_obj->main_pic = $this->input->post('fac_main_pic');
             $fac_obj->push_content = $this->input->post('fac_push');
             $fac_obj->ibeacon_id = $this->input->post('fac_ibeacon');
-            $fac_obj->update();
 
-            redirect('facilities');
+            // If DB update failed, then no need to upload files.
+            if (!$fac_obj->update()) {
+
+                $error_msg = $this->error_message('update_error');
+                log_message('debug', $error_msg);
+                echo $error_msg;
+
+            } else {
+
+                if (empty($_FILES['fac_main_pic'])) {
+
+                    $error_msg = $this->error_message->get_error_message('no_upload_file_error');
+                    log_message('error', $error_msg);
+                    echo $error_msg;
+
+                } else {
+
+                    // set upload config
+                    $config['upload_path'] = 'user_uploads';
+                    $config['allowed_types'] = 'gif|jpg|png';
+                    $config['max_size']	= '100000';
+                    $config['max_width']  = '2048';
+                    $config['max_height']  = '1536';
+                    $this->upload->initialize($config);
+
+                    $upload_results = $this->upload->do_multiple_upload('fac_main_pic');
+                    foreach ($upload_results as $result) {
+                        if (isset($result['error'])) {
+
+                            // if error is set, print why  upload failed.
+                            log_message('error', $result['name'].' upload failed.');
+                            echo $result['name'].' '.$result['error'];
+
+                        } else {
+                            log_message('debug', $result['name'].' uploaded.');
+                        }
+                    }
+                }
+            }
         }
     }
 
