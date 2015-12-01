65fd18063b52cd80eb36e356d16750014d7fe504
Chia-Ting <din1030@gmail.com>
2015-12-01 19:07:51 +0800

    update API

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -31,28 +31,46 @@ class App extends CI_Controller {
                     $this->load->model('Exhibition');
                     $obj_query = $this->Exhibition->select_where($ibeacon_info['link_obj_id']);
                     break;
+
                 case 'item':
                     $linked_obj['type'] = $ibeacon_info['link_type'];
                     $this->load->model('Item');
                     $obj_query = $this->Item->select_where($ibeacon_info['link_obj_id']);
-                    $obj_row = $obj_query->row();
-                    if (isset($obj_row->sec_id) && $obj_row->sec_id > 0) {
-                        $this->load->model('Section');
-                        $sec_query = $this->Section->select_where($obj_row->sec_id);
-                        $sec_info = $sec_query->result_array();
-                        $linked_obj['sec_info'] = $sec_info[0];
-                    }
                     break;
+
                 case 'fac':
                     $linked_obj['type'] = $ibeacon_info['link_type'];
                     $this->load->model('Facility');
                     $obj_query = $this->Facility->select_where($ibeacon_info['link_obj_id']);
                     break;
+
                 default:
                     return;
             }
-            $linked_obj['data'] = $obj_query->result_array();
-            $linked_obj['data'] = $linked_obj['data'][0];
+            $linked_obj_array = $obj_query->result_array();
+            $linked_obj['data'] = $linked_obj_array[0];
+
+            // appemd sec info
+            if (isset($linked_obj['data']['sec_id']) && $linked_obj['data']['sec_id'] > 0) {
+                $this->load->model('Section');
+                $sec_query = $this->Section->select_where($linked_obj['data']['sec_id']);
+                $sec_info = $sec_query->result_array();
+                $linked_obj['data']['sec_info'] = $sec_info[0];
+            }
+
+            // append custom field info
+            if ($linked_obj['type'] == 'item') {
+                $this->load->model('Custom_field');
+                $cfb_query = $this->Custom_field->select_where(array('item_id' => $linked_obj['data']['id'], 'type' => 'basic'));
+                if ($cfb_query->num_rows() > 0) {
+                    $linked_obj['data']['basic_field'] = $cfb_query->result_array();
+                }
+                $cfd_query = $this->Custom_field->select_where(array('item_id' => $linked_obj['data']['id'], 'type' => 'detail'));
+                if ($cfd_query->num_rows() > 0) {
+                    $linked_obj['data']['detail_field'] = $cfd_query->result_array();
+                }
+            }
+            
             echo json_encode($linked_obj);
         }
     }
